53b4975ff11d9470a5dd1385ce022d74878bc625
royalSH <38105748+royalSH@users.noreply.github.com>
2018-06-28 17:02:11 +0300

    Update hinainfo.js

--- commands/hinainfo.js
+++ commands/hinainfo.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
-    .setImage("https://i.imgur.com/fILPfin.png")
+    .setImage("https://i.imgur.com/McZp9re.png")
+    .setFooter(`Hinami | Informations Command  v1.2 |`, bot.user.displayAvatarURL)
     .setColor("#15f153")
 
     message.channel.send(botembed);

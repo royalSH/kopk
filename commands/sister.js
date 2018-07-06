0ee8985b567f96176d9da1668a9cb2a6bca057ef
royalSH <38105748+royalSH@users.noreply.github.com>
2018-07-06 20:15:24 +0300

    Update sister.js

--- commands/sister.js
+++ commands/sister.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id !== '291270997049344001') return message.reply("Du-te-n pula mea ca nu ti-am vazut moaca in viata mea.. :rage:");
+    if (message.author.id !== '291270997049344001') return message.reply("Nu te-am vazut in viata mea, cum sa-mi fi tu sora? :rage:");
     let coin = ['https://i.imgur.com/UqahYjh.png', 'https://i.imgur.com/cUUDzd6.png']
 let flip = Math.floor((Math.random() * coin.length));
 let coinembed = new Discord.RichEmbed()

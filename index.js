eaf281962a1c14d70f5bbb5f0fc0dfe81c9ffc4c
royalSH <38105748+royalSH@users.noreply.github.com>
2018-07-12 18:32:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -81,6 +81,39 @@ bot.on('message', msg => {
     .setImage("https://3.bp.blogspot.com/-8INJkDz5tIo/V-bI-cPcOeI/AAAAAAAAFoI/fEBVt4_VAn4YfJVQuk_Howg4vdJlfgyvACLcB/w1200-h630-p-k-no-nu/lup-in-blana-de-oaie.jpg")
     msg.channel.send(lupu2)
   }
+  if(msg.content.startsWith("#donate")) {
+      let link = ['paypal.me/hinamisrl'];
+      let warn = ['Ai grija la orice inselaciune, link-ul original este paypal.me/hinamisrl!'];
+      let a1 = ['Orice donatie ne ajuta la inbunatatirea robotului si al hostului, evident primesti si anumite beneficii pentru ajutorul oferit!'];
+      let a2 = ['Vei avea o comanda personala ce-ti va raspunde numai tie, vei primii o mentionare personalizata si bineinteles ca sugestia ta va fi pusa in considerare!']
+      let a3 = ['Din pacate banii vor fi pierduti iar noi nu vom oferii nimic, este raspunderea dvs. pentru banii pierduti, noi nu oferim nicio garantie!'];
+      let a4 = ['Bineinteles ca noi te vom adauga in blacklist-ul nostru deoarece este un gest prostesc si fara noima ce va da dureri de cap echipei deoarece pierdem timp in a face beneficiile pentru donatia falsa!'];
+      let a5 = ['Beneficiile vor fi primite in cam 20 de minute(depinde de echipa), dar nu exista ca cineva care ne-a sustinut sa nu-si primeasca beneficiile'];
+      let a6 = ['Deocamdata acceptam doar paypal si pin-uri de paysafecard.'];
+      let a2_5 = ['Bineinteles, te vom rasplatii cu gradul de patron pentru donatia ta!'];
+      let a7 = ['Banii stransi vor fi bagati intr-un dedicat mai performant si in achizitionarea a unor sisteme complexe distribuite de oameni profesionisti din strainatate!'];
+      let a0 = ['Nu avem nicio limita de donatie, orice donatie este bine-venita!']
+
+      let dix = ['Sunt foarte fericit c-am reusit s-o aduc pe Hinami la o versiune cat de cat stabila ce reuseste sa se mentina!'];
+      let anom = ['Sunt la fel de fericit ca Developerul pentru c-am reusit sa ajut la dezvoltarea acestui robot de discord, probabil ca dupa terminarea ei, o voi adauga la serverul meu oficial!']
+
+  let donate = new Discord.RichEmbed()
+    .setTitle("DONATE COMMAND")
+    .setDescription("Daca vrei sa vezi robotul in stadiul stabil ne poti sustine prin donatia ta(orice suma), noi iti vom oferii diferite beneficii pentru ajutorul acordat de tine!")
+    .addField(`LINK:`, `${link}`)
+    .addField(`:warning:ATENTIE!`, `${a1}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Ce primesc pentru donatia oferita?`, `${a2}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Cat trebuie sa donez?`, `${a0}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Voi avea grad pe serverul oficial pentru ca am donat?`, `${a2_5}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Ce se intampla daca am donat cui nu trebuie?`, `${a3}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Ce se intampla daca fac fake-donate?`, `${a4}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Ce se intampla daca donez si nu primesc beneficiile?`, `${a5}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Mai exista si alte metode de plata?`, `${a6}`)
+    .addField(`:diamond_shape_with_a_dot_inside:Unde se duc banii stransi din donatii?`, `${a7}`)
+    .setFooter("Hinami si-a inceput vanatoarea de patroni, daca vrei s-o sustii in acest traseu al dezvoltarii ii poti dona orice suma esti dispus/a sa ii dai.", bot.user.displayAvatarURL)
+
+    msg.channel.send(donate)
+  }
 });
 
 bot.on("message", async message => {

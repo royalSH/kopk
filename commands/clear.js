56d8f6012259ab454e40a41c2d8aa97bc278ab58
royalSH <38105748+royalSH@users.noreply.github.com>
2018-07-12 15:48:19 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("N-ai acces..");
+  if(!args[0]) return message.channel.send("/clear <nr.mess> ce-i asa de greu?");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`**:wastebasket: | Am sters ${args[0]} mesaje/mesaj!**`).then(msg => msg.delete(2000));
 });

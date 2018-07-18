b504b4c0354333d9d6985b29e20cd0fcb23ce53f
royalSH <38105748+royalSH@users.noreply.github.com>
2018-07-18 17:42:16 +0300

    Update bigass.js

--- commands/bigass.js
+++ commands/bigass.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    if (!message.channel.nsfw) return message.channel.send("**:exclamation: | Comanda poate fi executata doar intr-un channel NSFW**");
     let replies = ["https://cdn.boob.bot/pawg/5CBA.jpg", "https://cdn.boob.bot/pawg/4A44.jpg", "https://i.imgur.com/JYk7NiJ.png", "https://i.imgur.com/6eMFDyG.png", "https://i.imgur.com/V2MRHzR.png", "https://i.imgur.com/vfPa4ng.jpg", "https://i.imgur.com/M2SlhtV.png", "https://i.imgur.com/pYOGIK8.png", "https://i.imgur.com/oM7kEL5.jpg", "https://i.imgur.com/pQiOFRx.png", "https://i.imgur.com/Raz6EKR.png", "https://i.imgur.com/x1WqJfv.png", "https://i.imgur.com/Czk7YlX.jpg"];
-
     let result = Math.floor((Math.random() * replies.length));
 
     let pawgembed = new Discord.RichEmbed()

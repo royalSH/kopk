467b0c9944eb475dc550508378579f6d0170fe04
royalSH <38105748+royalSH@users.noreply.github.com>
2018-08-03 16:25:16 +0300

    Update invitelink.js

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("INVITE CODE: https://discord.gg/9C4eHXb")
+    .setDescription("INVITE CODE: discord.me/hinami")
     .setFooter(`Hinami | Official Server |`, bot.user.displayAvatarURL)
     .setColor("#15f153")
 

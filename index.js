84b5938d3722678033732cfd1675fe31b450d7e9
royalSH <38105748+royalSH@users.noreply.github.com>
2018-08-15 16:34:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ bot.on('message', msg => {
     let random = Math.floor((Math.random() * reply.length));
     msg.channel.send(reply[random])
   }
+  if (msg.content === "dumnezeu" || msg.content === "Dumnezeu" || msg.content === "DUMNEZEU") {
+  msg.channel.send("Este ocupat nepasandu-i de Africa, probabil te poate ajuta Luci.")
+  }
   if (msg.content === 'tigani') {
     msg.channel.send('Pe rug cu iei ca in Game Of Thrones!!!!')
   }
